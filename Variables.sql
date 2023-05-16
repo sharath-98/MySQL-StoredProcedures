3d83f4c7ca00c288b44d4e0d4db6a0aa936f2a7c
sharath-98 <sharathchander.p@gmail.com>
2023-05-16 17:57:29 -0500

    Variable assignment

--- Variables.sql
+++ Variables.sql
@@ -1,7 +1,8 @@
 DELIMITER $$
 	CREATE PROCEDURE VARIABLE_GET_STUDENTS()
 		BEGIN
-			declare studentId int default 2;
+			declare studentId int default 1;
+            set studentId = 3;
 			select * from student where student_id = studentId;
         END $$
 DELIMITER ;
