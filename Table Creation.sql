3095c979a83e520680b0b29455e79973d53ad719
Sharath Chander Pugazhenthi <sharathchander.p@gmail.com>
2023-05-16 17:23:07 -0500

    Update Table Creation.sql

--- Table Creation.sql
+++ Table Creation.sql
@@ -1,8 +1,4 @@
 use sharath_chander_pugazhenthi;
 select * from student;
 
-insert into student values(2,'Pugazhenthi','Vishal', 2004, 'vp@gmail.com','Bangalore');
-insert into student values(3,'PT','Chithra', 1970, 'ptChithra@gmail.com','Bangalore');
-insert into student values(4,'P','Pugazhenthi', 1971, 'pugazhenthi@gmail.com','Bangalore');
-insert into student values(5,'Sharath','Soundarya', 1998, 'SS@gmail.com','Bangalore');
 
