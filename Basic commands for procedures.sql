40f9cce289047a25b910da5f5173a18fb5de6096
sharath-98 <sharathchander.p@gmail.com>
2023-05-17 13:53:07 -0500

    Basic Commands for procedure

--- Basic commands for procedures.sql
+++ Basic commands for procedures.sql
@@ -6,3 +6,9 @@ SHOW PROCEDURE STATUS WHERE DB = 'sharath_chander_pugazhenthi';
 
 -- show procedures that match a certain pattern
 SHOW PROCEDURE STATUS WHERE name like 'IF_%';
+
+-- SHOW PROCEDURE
+SHOW create procedure get_students;
+
+-- DROP PROCEDURE
+DROP procedure IF EXISTS get_students;
